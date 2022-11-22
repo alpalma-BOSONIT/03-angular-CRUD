a8cbedc199b5dd691a19cc3cd76a82fe6d4066bf
Álvaro Palma Mancheño <alvaro.palma@bosonit.com>
2022-11-22 17:08:25 +0100

    add user implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { TableComponent } from './components/table/table.component';
+import { RenderTruePipe } from './pipes/render-true.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FormComponent, TableComponent, RenderTruePipe],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,90 @@
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { User } from 'src/app/interfaces/user.interface';
+import { Operation } from 'src/app/types/operation';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent implements OnInit, DoCheck {
+  countryOptions: string[] = ['Ireland', 'Australia', 'Spain', 'Guatemala'];
+  users: User[] = [];
+  userToUpdate: User = {
+    id: 0,
+    username: '',
+    password: '',
+    passwordConfirm: '',
+    email: '',
+    subscribe: false,
+    country: '',
+    city: '',
+  };
+  operationType: Operation = 'create';
+  btnText: string = this.operationType === 'create' ? 'Create' : 'Update';
+
+  userForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirm: new FormControl(''),
+    email: new FormControl(''),
+    subscribe: new FormControl(false),
+    country: new FormControl(''),
+    city: new FormControl(''),
+  });
+
+  constructor(private us: UserService) {}
+
+  ngOnInit(): void {
+    this.us.users$.subscribe((users) => (this.users = users));
+    this.us.operationType$.subscribe((type) => (this.operationType = type));
+    this.us.userToUpdate$.subscribe((user) => (this.userToUpdate = user));
+  }
+
+  ngDoCheck(): void {
+    // const user: User = { ...this.userToUpdate };
+    if (this.operationType === 'update') {
+      this.userForm.setValue({
+        username: this.userToUpdate.username,
+        password: this.userToUpdate.password,
+        passwordConfirm: this.userToUpdate.passwordConfirm,
+        email: this.userToUpdate.email,
+        subscribe: this.userToUpdate.subscribe,
+        country: this.userToUpdate.country,
+        city: this.userToUpdate.city,
+      });
+    }
+  }
+
+  resetForm(): void {
+    this.userForm.setValue({
+      username: '',
+      password: '',
+      passwordConfirm: '',
+      email: '',
+      subscribe: false,
+      country: '',
+      city: '',
+    });
+  }
+
+  addUser(): void {
+    this.us.addUser({ id: Date.now(), ...this.userForm.value });
+    this.resetForm();
+    console.log(this.users);
+  }
+
+  updateUser(): void {
+    this.us.updateUser({
+      id: this.userToUpdate.id,
+      ...this.userForm.value,
+    });
+  }
+
+  onSubmit(): void {
+    if (this.operationType === 'create') this.addUser();
+    if (this.operationType === 'update') this.updateUser();
+  }
+}
